91591527b0701d977fc7b7392b9ea7058918b3ab
bread <chalida.bread@gmail.com>
2024-01-21 22:25:24 +0700

    refactor:use react hook for greeting message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState("Greeting Message");
+  const [greetingInput, setGreetingInput] = useState("");
+
+  const handleMesageChange = (event) => {
+    setGreetingInput(event.target.value);
+  };
+
+  const handleClick = () => {
+    setGreetingMessage(greetingInput);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={handleMesageChange}
+          value={greetingInput}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleClick}>Update text</button>
       </div>
     </div>
   );
